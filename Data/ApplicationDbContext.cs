8245a4700cf6fd180facdaefe251482542df3360
Samuel Midgley <midgley.sam@gmail.com>
2021-11-21 20:27:40 +0000

    First Commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RiddleDatabase.Models;
+
+namespace RiddleDatabase.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<RiddleDatabase.Models.Riddle> Riddle { get; set; }
+    }
+}
